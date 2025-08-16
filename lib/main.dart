1800f08ad51d971579d6ad4bab30c817a0521f63
ruslanflutterdev <rusrus1984_8@mail.ru>
2025-08-16 10:34:43 +0500

    Разбираюсяс отправÐкой

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
