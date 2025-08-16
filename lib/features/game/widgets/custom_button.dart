ff05c157a3d9b65809666cf8766af8d841c89e03
ruslanflutterdev <rusrus1984_8@mail.ru>
2025-08-16 11:11:23 +0500

    сделал универсальную кнопку для всего приложения

---
+++ lib/features/game/widgets/custom_button.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const CustomButton({super.key, required this.text, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          padding: EdgeInsets.symmetric(vertical: 16),
+          textStyle: TextStyle(fontSize: 18),
+        ),
+        child: Text(text),
+      ),
+    );
+  }
+}
